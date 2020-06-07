7e9d01070e481649a54c988573d300762968ab56
cilach <demadima1601@gmail.com>
2020-06-08 01:22:07 +0300

    v2.3 CreateAccountForm

--- simpleweb/java/com/javamaster/controller/HomeServlet.java
+++ simpleweb/java/com/javamaster/controller/HomeServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
+import javax.faces.validator.LengthValidator;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -18,7 +19,7 @@ public class HomeServlet extends HttpServlet {
 		String path = request.getServletPath();
 		if (path.equals("/")) {
 			request.getRequestDispatcher("/WEB-INF/view/index.jsp").forward(request, response);
-		}else if (path.equals("/createAccount")) {
+		} else if (path.equals("/createAccount")) {
 			request.getRequestDispatcher("/WEB-INF/view/createAccount.jsp").forward(request, response);
 		} else if (path.equals("/welcome")) {
 			ArrayList<Notebook> products = PropertiesDB.select();
@@ -28,23 +29,21 @@ public class HomeServlet extends HttpServlet {
 		} else if (path.equals("/create")) {
 			getServletContext().getRequestDispatcher("/WEB-INF/view/create.jsp").forward(request, response);
 
-		}else if (path.equals("/edit")) {
+		} else if (path.equals("/edit")) {
 			try {
-	            int id = Integer.parseInt(request.getParameter("id"));
-	            Notebook product = PropertiesDB.selectOne(id);
-	            if(product!=null) {
-	                request.setAttribute("product", product);
-	                getServletContext().getRequestDispatcher("/WEB-INF/view/edit.jsp").forward(request, response);
-	            }
-	            else {
-	                getServletContext().getRequestDispatcher("/WEB-INF/view/notfound.jsp").forward(request, response);
-	            }
-	        }
-	        catch(Exception ex) {
-	            getServletContext().getRequestDispatcher("/WEB-INF/view/notfound.jsp").forward(request, response);
-	        }
-		}else  {
-			 getServletContext().getRequestDispatcher("/WEB-INF/view/notfound.jsp").forward(request, response);
+				int id = Integer.parseInt(request.getParameter("id"));
+				Notebook product = PropertiesDB.selectOne(id);
+				if (product != null) {
+					request.setAttribute("product", product);
+					getServletContext().getRequestDispatcher("/WEB-INF/view/edit.jsp").forward(request, response);
+				} else {
+					getServletContext().getRequestDispatcher("/WEB-INF/view/notfound.jsp").forward(request, response);
+				}
+			} catch (Exception ex) {
+				getServletContext().getRequestDispatcher("/WEB-INF/view/notfound.jsp").forward(request, response);
+			}
+		} else {
+			getServletContext().getRequestDispatcher("/WEB-INF/view/notfound.jsp").forward(request, response);
 		}
 	}
 
@@ -58,10 +57,10 @@ public class HomeServlet extends HttpServlet {
 				int age = Integer.parseInt(request.getParameter("age"));
 				String sex = request.getParameter("sex");
 				String phone = request.getParameter("phone");
-				
+
 				Notebook product = new Notebook(surname, name, age, sex, phone);
 				PropertiesDB.insert(product);
-				response.sendRedirect(request.getContextPath()+"/welcome");
+				response.sendRedirect(request.getContextPath() + "/welcome");
 			} catch (Exception ex) {
 
 				getServletContext().getRequestDispatcher("/WEB-INF/view/create.jsp").forward(request, response);
@@ -69,85 +68,94 @@ public class HomeServlet extends HttpServlet {
 		}
 		if (path.equals("/edit")) {
 			try {
-	            int id = Integer.parseInt(request.getParameter("id"));
-	            String surname = request.getParameter("surname");
+				int id = Integer.parseInt(request.getParameter("id"));
+				String surname = request.getParameter("surname");
 				String name = request.getParameter("name");
 				int age = Integer.parseInt(request.getParameter("age"));
 				String sex = request.getParameter("sex");
 				String phone = request.getParameter("phone");
-				
-	            Notebook product =new Notebook(id ,surname, name, age, sex, phone);
-	            PropertiesDB.update(product);
-	            response.sendRedirect(request.getContextPath()+"/welcome");
-	        }
-	        catch(Exception ex) {
-	             
-	            getServletContext().getRequestDispatcher("/WEB-INF/view/notfound.jsp").forward(request, response);   
-	        }
-			
+
+				Notebook product = new Notebook(id, surname, name, age, sex, phone);
+				PropertiesDB.update(product);
+				response.sendRedirect(request.getContextPath() + "/welcome");
+			} catch (Exception ex) {
+
+				getServletContext().getRequestDispatcher("/WEB-INF/view/notfound.jsp").forward(request, response);
+			}
+
 		}
-		if(path.equals("/delete")) {
-			 try {
-		            int id = Integer.parseInt(request.getParameter("id"));          
-		            PropertiesDB.delete(id);
-		            response.sendRedirect(request.getContextPath()+"/welcome");
-		        }
-		        catch(Exception ex) {
-		            getServletContext().getRequestDispatcher("/WEB-INF/view/notfound.jsp").forward(request, response);
-		        }
+		if (path.equals("/delete")) {
+			try {
+				int id = Integer.parseInt(request.getParameter("id"));
+				PropertiesDB.delete(id);
+				response.sendRedirect(request.getContextPath() + "/welcome");
+			} catch (Exception ex) {
+				getServletContext().getRequestDispatcher("/WEB-INF/view/notfound.jsp").forward(request, response);
+			}
 		}
-		
+
 		if (path.equals("/")) {
 			try {
 				ArrayList<Account> products = PropertiesDB.selectAccount();
 				Account product_by_id = null;
-				boolean key = false;// ключ , позволяющий узнать, правильный был введен ли пароль  и mail
-				String mail = request.getParameter("mail"); 
-				String password = request.getParameter("password"); 
+				boolean key = false;// ключ , позволяющий узнать, правильный был введен ли пароль и mail
+				String mail = request.getParameter("mail");
+				String password = request.getParameter("password");
 				// Проверка на совпадение mail и password
-				for(Account product : products) {					
-					if(product.getMail().equals(mail) && product.getPassword().equals(password)) {
-						key= true;
+				for (Account product : products) {
+					if (product.getMail().equals(mail) && product.getPassword().equals(password)) {
+						key = true;
 						product_by_id = product;
 					}
 				}
-				if(key) {
+				if (key) {
 					request.setAttribute("product", product_by_id);
-					response.sendRedirect(request.getContextPath()+"/welcome");
-					
-				}else {
-					getServletContext().getRequestDispatcher("/WEB-INF/view/index.jsp").forward(request, response);  
+					response.sendRedirect(request.getContextPath() + "/welcome");
+
+				} else {
+					getServletContext().getRequestDispatcher("/WEB-INF/view/index.jsp").forward(request, response);
 				}
-			
-				
-	        }
-	        catch(Exception ex) {
-	             
-	            getServletContext().getRequestDispatcher("/WEB-INF/view/index.jsp").forward(request, response);   
-	        }
-			
+
+			} catch (Exception ex) {
+
+				getServletContext().getRequestDispatcher("/WEB-INF/view/index.jsp").forward(request, response);
+			}
+
 		}
-		
-		
+
 		if (path.equals("/createAccount")) {
-			
+			int count = 0;
 			try {
+
+				String mail = request.getParameter("mail");
+				String password = request.getParameter("password");
+				ArrayList<Account> products = PropertiesDB.selectAccount();
+				// Проверка на уже существующий мейл + пустые строки
+				for (Account product : products) {
+					count ++;
+						if (product.getMail().equals(mail)) {
+							response.sendRedirect(request.getContextPath() + "/createAccount");
+								break;							
+						}else if (count == products.size()) {
+							if (mail.length() == 0 || password.length() == 0) {
+
+								response.sendRedirect(request.getContextPath() + "/createAccount");
+							} else {
+								Account prod= new Account(mail, password);
+
+								PropertiesDB.insert(prod);
+								response.sendRedirect(request.getContextPath() + "/");
+							}
+							
+						}
+						
+				}
 				
-					String mail = request.getParameter("mail");
-					String password = request.getParameter("password");
-				
-			//	if(!mail.equals("") || !password.equals("") ) {
-					Account product = new Account(mail, password);
-				
-					PropertiesDB.insert(product);
-					response.sendRedirect(request.getContextPath()+"/");
-				//}
-	        }
-	        catch(Exception ex) {
-	             
-	            getServletContext().getRequestDispatcher("/WEB-INF/view/createAccount.jsp").forward(request, response);   
-	        }
+			} catch (Exception ex) {
+
+				getServletContext().getRequestDispatcher("/WEB-INF/view/createAccount.jsp").forward(request, response);
+			}
 		}
-		
+
 	}
 }
